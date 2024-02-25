c9c7b4e5c77827788b83d8db04748715429eeac3
cybr2 <ydoowbernisca2@gmail.com>
2024-02-25 18:54:07 +0800

    add login, add signup

---
+++ components/Logo.js
@@ -0,0 +1,12 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+import LogoIcon from '../assets/svgs/logo-blue.svg';
+
+
+const Logo = ({size}) => {
+  return (
+    <LogoIcon height={size} width={size} />
+  )
+}
+
+export default Logo
