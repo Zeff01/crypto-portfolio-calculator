2edab95ef5e121f599890c3eb552e093edd4f912
cybr2 <ydoowbernisca2@gmail.com>
2024-03-14 21:16:58 +0800

    fix the dev environment for cmc api, key and supabase

--- services/supabase.js
+++ services/supabase.js
@@ -4,8 +4,11 @@ import 'react-native-url-polyfill/auto'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL
-const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_KEY
+// const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL
+// const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_KEY
+
+const supabaseUrl = 'https://isfrtwqvpykkwfrpcmjy.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzZnJ0d3F2cHlra3dmcnBjbWp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg3ODg3NzAsImV4cCI6MjAyNDM2NDc3MH0.b9-urLONFJ50eu3OsW0Xr94yHjzRSwsYivNHn2R5lSI'
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
     auth: {
